214b414db602c282baf74b8f3e9b26e781cadda3
Cara Bell <cara.bell@192.168.1.129>
2021-03-03 16:00:07 +0000

    Add associations and validations'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :posts, through: :comments, dependent: :destroy
+  has_many :authors, through: :posts, dependent: :destroy
+
+  validates :name, uniqueness: { case_sensitive: false }
 end
